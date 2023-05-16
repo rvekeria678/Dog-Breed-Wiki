2556f0ac47eef08f3e9c50764861c98a414718d4
Raj Vekeria <81255193+rvekeria678@users.noreply.github.com>
2023-05-16 14:09:00 -0400

    Added Dev Tools and Homepage

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/index.html"
+  ],
+  theme: {
+    fontFamily: {
+      'outfit': ['Outfit', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
